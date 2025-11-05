12729a769975923aef5db952f5a9347fbf5df712
Nagaraj S <nagarajs2016@gmail.com>
2025-11-05 12:37:53 +0000

    remove it (_for element <CardContent>_)

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useEffect, useState } from 'react';
@@ -21,14 +22,6 @@ export default function TransactionsPage() {
     <div className="space-y-4">
        <div className="flex items-center justify-between">
         <h1 className="font-headline text-2xl font-semibold">All Transactions</h1>
-        <Card className="text-right">
-            <CardHeader className="p-2 pb-0">
-                <CardDescription>Total Transactions</CardDescription>
-            </CardHeader>
-            <CardContent className="p-2 pt-0">
-                <p className="text-2xl font-bold">{allTransactions.length}</p>
-            </CardContent>
-        </Card>
        </div>
       <DataTable columns={columns} data={allTransactions} />
     </div>

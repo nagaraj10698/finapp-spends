413223f7da3ed0c46bb875fe359259ddcb5689e1
Nagaraj S <nagarajs2016@gmail.com>
2025-11-05 12:00:59 +0000

    check on this, system should be capable if user added 600 rows of transa

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  serverActions: {
+    bodySizeLimit: '2mb', // Default is 1mb
+    maxDuration: 30, // Default is 60s, but for some providers it's less. Let's set it explicitly.
+  },
 };
 
 export default nextConfig;

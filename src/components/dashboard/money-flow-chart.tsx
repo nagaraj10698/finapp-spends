bb9b9b213f6fece2ec9f18b5d1ae37259f50e9b3
Nagaraj S <nagarajs2016@gmail.com>
2025-11-07 06:38:44 +0000

    increate the width of the column chart in money flow and show box shappe

--- src/components/dashboard/money-flow-chart.tsx
+++ src/components/dashboard/money-flow-chart.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { Bar, BarChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis, Legend } from 'recharts';
@@ -30,7 +31,7 @@ export default function MoneyFlowChart({ data }: MoneyFlowChartProps) {
   return (
     <ChartContainer config={chartConfig} className="min-h-[250px] w-full">
       <ResponsiveContainer width="100%" height={250}>
-        <BarChart accessibilityLayer data={data} margin={{ top: 20, right: 0, left: -20, bottom: 5 }}>
+        <BarChart accessibilityLayer data={data} margin={{ top: 20, right: 0, left: -20, bottom: 5 }} barCategoryGap="20%">
           <XAxis
             dataKey="name"
             tickLine={false}
@@ -50,8 +51,8 @@ export default function MoneyFlowChart({ data }: MoneyFlowChartProps) {
                 />
             }
           />
-          <Bar dataKey="income" fill="var(--color-income)" radius={4} />
-          <Bar dataKey="expense" fill="var(--color-expense)" radius={4} />
+          <Bar dataKey="income" fill="var(--color-income)" />
+          <Bar dataKey="expense" fill="var(--color-expense)" />
         </BarChart>
       </ResponsiveContainer>
     </ChartContainer>
